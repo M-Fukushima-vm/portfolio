0072115328db4b544cd67158e55928f8e1310cc3
M-Fukushima-vm <m.tech1980s@gmail.com>
2021-07-20 22:51:44 +0900

    signup実装完了

---
+++ db/migrate/20210720134305_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name, presence: true
+      t.string :email, presence: true, uniqueness: true
+      t.string :password_digest, presence: true
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
