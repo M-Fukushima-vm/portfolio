e19f30756a6896ef15e8fea379c52f081499668a
M-Fukushima-vm <m.tech1980s@gmail.com>
2021-07-21 13:16:31 +0900

    signup_resデータ(user_serializer)実装完了

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,7 @@ class Api::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.save!
+    render json: user, serializer: UserSerializer
   end
 
   private
