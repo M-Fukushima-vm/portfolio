d75f82ebd415469b982375fe1c53c55a227bf7bd
M-Fukushima-vm <m.tech1980s@gmail.com>
2021-08-05 21:38:37 +0900

    日本語フォント導入・カラー調整

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id="app">
+  <v-app id="app" :style="{ background: 'linear-gradient(165deg, #fdfdfd 0%, #edeff1 100%)' }">
+    <p class="primary--text">{{ message }}</p>
     <p>{{ message }}</p>
     <v-btn small>ボタン</v-btn>
-  </div>
+    <v-btn small color="primary">Primary</v-btn>
+    <v-btn small color="accent">Accent</v-btn>
+    <v-btn small color="secondary">Secondary</v-btn>
+    <v-btn small color="error">Error</v-btn>
+    <v-btn small color="warning">Warning</v-btn>
+    <v-btn small color="info">Info</v-btn>
+    <v-btn small color="success">Success</v-btn>
+  </v-app>
 </template>
 
 <script>
